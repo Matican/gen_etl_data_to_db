570736e821b8324179998c0e687c46667530888c
Matican <31931856+Matican@users.noreply.github.com>
2021-04-30 14:35:47 +1000

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,37 @@
+# User instruction:
+# ---Create postgres database like [stream_test] below
+# ---Create postgres table like [transaction] below 
+# ---Edit line 7, optionally.
+# ---First, run mock_api.  
+# ---Then, run this app.
+
+
+import requests
+import psycopg2
+
+# Edit the number at end of the URL to specify number of rows 
+with requests.get("http://127.0.0.1:5000/tickle_my_belly/1000", stream=True) as r:
+
+    conn = psycopg2.connect(dbname="stream_test", user="postgres", password=".")
+    cur = conn.cursor()
+    sql = "INSERT INTO transactions (userid, userkey, amount) VALUES  (%s, %s, %s)"
+
+    
+    buffer = ""
+    for chunk in r.iter_content(chunk_size=1):
+        if chunk.endswith(b"\n"):
+            t = eval(buffer)
+            print(t)
+            if t[2] > 900:
+                print("Fine customer...!")
+            cur.execute(sql, (t[0], t[1], t[2]))
+            conn.commit()
+            buffer = ""
+        else: 
+            buffer += chunk.decode()
+
+
+
+
+
+
---
+++ mock_api.py
@@ -0,0 +1,24 @@
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+APP = Flask(__name__)
+
+# To specify rowcount, add the [/tickle_my_belly/<int:rowcount>] to your localhost.
+
+@APP.route("/tickle_my_belly/<int:rowcount>", methods=["GET"])
+def get_data(rowcount):
+
+    def f():
+        for _i in range(rowcount):
+            time.sleep(.01)
+            field_1 = uuid.uuid4()
+            print(field_1)
+            field_2 = uuid.uuid4()
+            amount = round(random.uniform(-1000, 1000), 2)
+            yield f"('{field_1}', '{field_2}', {amount}) \n"
+    return Response(stream_with_context(f()))
+
+if __name__ == "__main__":
+    APP.run(debug=True)
